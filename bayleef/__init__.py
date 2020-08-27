5a07e89be0f8487e2de43c1c5c4a60a2d22b79d7
mark <mol3earth@gmail.com>
2020-08-27 13:27:22 -0700

    added functions to check config file and throw Errors

--- bayleef/__init__.py
+++ bayleef/__init__.py
@@ -51,14 +51,50 @@ class dotdict(dict):
      __setattr__ = dict.__setitem__
      __delattr__ = dict.__delitem__
 
+def check_file_or_path(path, file=False):
+    """checks if file or path exists."""
+    if not path:
+        return False, f"is not set"
+    if not os.path.exists(path):
+        return False, f"path '{path}' does not exist"
+    if file and not os.path.isfile(path):
+        return False, f"file '{path}' does not exist"
+    return True, ''
+
+def loop_params(config, params, not_ok, errors, file=False):
+    """loops over keys in 'params' in the config dict."""
+    for param in params:
+        ok, msg = check_file_or_path(config[param], file)
+        if not ok:
+            not_ok = True
+            errors.append(f"\t'{param}' {msg}.")
+    return not_ok, errors    
+
+def config_check(config, config_path):
+    """checks if the paths in the config.yaml file exist"""
+    not_ok = False
+    errors = [f"Your configuration file {config_path} is not configured properly:"]
+    warnings = [f"WARNING: Your configuration file {config_path} is missing some settings.",
+        "This may result in unexpected behavior or errors later."]
+    # I am assuming the config file location and keys do not need to be checked. 
+    # The copyfile command below should throw an exception so we don't need to here.
+    not_ok, errors = loop_params(config, ['data', 'davinci_bin'], not_ok, errors)
+    not_ok, errors = loop_params(config['themis'], ['map_file'], not_ok, errors, file=True)
+    warn, warnings = loop_params(config['themis'], ['controlled_kernels'], False, warnings)
+    if not_ok:
+        if warn: 
+            errors += warnings
+        raise USGSError('\n'.join(errors))
+    if warn:
+        print('\n'.join(warnings))
+
 config_file = os.path.join(get_path('config'), 'config.yaml')
 config = None
 
 os.makedirs(os.path.join(str(Path.home()), ".bayleef"), exist_ok=True)
-if not os.path.isfile(os.path.join(str(Path.home()), ".bayleef", "config.yaml")):
-    copyfile(config_file, os.path.join(str(Path.home()), ".bayleef", "config.yaml"))
-    config = dotdict(yaml.load(open(config_file), Loader=yaml.BaseLoader))
-else:
-    config = dotdict(yaml.load(open(os.path.join(str(Path.home()), ".bayleef", "config.yaml")), Loader=yaml.BaseLoader))
-
-config_file = os.path.join(str(Path.home()), ".bayleef", "config.yaml")
+config_path = os.path.join(str(Path.home()), ".bayleef", "config.yaml")
+if not os.path.isfile(config_path):
+    copyfile(config_file, config_path)
+config = dotdict(yaml.load(open(config_path), Loader=yaml.BaseLoader))
+config_check(config, config_path)
+config_file = config_path
