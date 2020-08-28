cfc63332ce2890ea9a3aa82fcf8d845696a178b6
mark <mol3earth@gmail.com>
2020-08-28 12:42:22 -0700

    Cleaned up config check functions for readability.

--- bayleef/__init__.py
+++ bayleef/__init__.py
@@ -54,38 +54,37 @@ class dotdict(dict):
 def check_file_or_path(path, file=False):
     """checks if file or path exists."""
     if not path:
-        return False, f"is not set"
+        return False, f"is missing or not set"
     if not os.path.exists(path):
         return False, f"path '{path}' does not exist"
     if file and not os.path.isfile(path):
         return False, f"file '{path}' does not exist"
     return True, ''
 
-def loop_params(config, params, not_ok, errors, file=False):
+def loop_params(config, params, file=False):
     """loops over keys in 'params' in the config dict."""
+    errors = []
     for param in params:
-        ok, msg = check_file_or_path(config[param], file)
+        ok, msg = check_file_or_path(config.get(param, ''), file)
         if not ok:
-            not_ok = True
             errors.append(f"\t'{param}' {msg}.")
-    return not_ok, errors    
+    return errors    
 
 def config_check(config, config_path):
     """checks if the paths in the config.yaml file exist"""
-    not_ok = False
     errors = [f"Your configuration file {config_path} is not configured properly:"]
     warnings = [f"WARNING: Your configuration file {config_path} is missing some settings.",
         "This may result in unexpected behavior or errors later."]
-    # I am assuming the config file location and keys do not need to be checked. 
-    # The copyfile command below should throw an exception so we don't need to here.
-    not_ok, errors = loop_params(config, ['data', 'davinci_bin'], not_ok, errors)
-    not_ok, errors = loop_params(config['themis'], ['map_file'], not_ok, errors, file=True)
-    warn, warnings = loop_params(config['themis'], ['controlled_kernels'], False, warnings)
-    if not_ok:
-        if warn: 
+    # I am assuming the config file location does not get checked here.
+    # The copyfile command below should throw an exception below so it would be redundant.
+    errors += loop_params(config, ['data', 'davinci_bin'])
+    errors += loop_params(config['themis'], ['map_file'], file=True)
+    warnings += loop_params(config['themis'], ['controlled_kernels'])
+    if len(errors)>1:
+        if len(warnings)>2:
             errors += warnings
         raise USGSError('\n'.join(errors))
-    if warn:
+    if len(warnings)>2:
         print('\n'.join(warnings))
 
 config_file = os.path.join(get_path('config'), 'config.yaml')
